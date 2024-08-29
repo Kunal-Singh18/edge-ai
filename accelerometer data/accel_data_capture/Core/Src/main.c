54180e108cf59f519f4c19d46f0c8abb67586abc
Kunal-Singh18 <kunalsingh92724@gmail.com>
2024-08-29 21:28:11 +0530

    python

--- accelerometer data/accel_data_capture/Core/Src/main.c
+++ accelerometer data/accel_data_capture/Core/Src/main.c
@@ -32,6 +32,9 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 
+/*accelerometer module address */
+#define AccelAddress (0x53<<1)
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -46,6 +49,14 @@ UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 
+uint8_t  Rx_buffer[4];
+uint8_t  TS_buffer[4]={0,0,0,0};
+uint8_t CheckSum =0;
+uint32_t TimeStamp;
+uint8_t SendFlag  = 0;
+uint8_t AccelData[6];
+uint8_t chipid=0;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -59,6 +70,85 @@ static void MX_USART2_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+/* Write the Value into corresponding register address */
+void AccelWrite (uint8_t reg, uint8_t value)
+{
+	uint8_t data[2];
+	data[0] = reg;
+	data[1] = value;
+	HAL_I2C_Master_Transmit (&hi2c1, AccelAddress, data, 2, 100);
+}
+
+/* Read the values from corresponding register address */
+void AccelReadValues (uint8_t reg , uint8_t numberofbytes)
+{
+	HAL_I2C_Mem_Read (&hi2c1, AccelAddress, reg, 1, (uint8_t *)AccelData, numberofbytes, 100);
+}
+
+/* Read the address of chip */
+void AccelReadAddress (uint8_t reg)
+{
+	HAL_I2C_Mem_Read (&hi2c1, AccelAddress, reg, 1, &chipid, 1, 100);
+
+}
+
+/* initialize the accelerometer */
+void AccelInit (void)
+{
+	AccelReadValues(0x00 ,1);
+	AccelWrite (0x2d, 0x00);  // reset all bits
+	AccelWrite (0x2d, 0x08);  // power_cntl measure and wake up 8hz*/
+	AccelWrite (0x31, 0x00);  // data_format range= +- 2g
+	//AccelWrite (0x2c, 0x0d);  // 800 hz output data rate
+
+}
+
+/* UART receive interrupt call back */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+
+		if (huart->Instance == USART2)             //is current uart?
+		{
+			/* check for the correct received confirmation code */
+			if((Rx_buffer[0] == 0xFF) && (Rx_buffer[1] == 0x00) && (Rx_buffer[2] == 0xFF) && (Rx_buffer[3] == 0x00))
+			{
+				/*Set send flag to 1*/
+				SendFlag = 1;
+
+			}else
+			{
+            /* Transmit the received time stamp */
+			HAL_UART_Transmit(&huart2 ,Rx_buffer ,4 ,20);
+
+			/*Convert timestamp from bytes to integer */
+			TimeStamp |= (Rx_buffer[0]);
+			TimeStamp =  (TimeStamp << 8);
+			TimeStamp |= (Rx_buffer[1]);
+			TimeStamp =  (TimeStamp << 8);
+			TimeStamp |= (Rx_buffer[2]);
+			TimeStamp =  (TimeStamp << 8);
+			TimeStamp |= (Rx_buffer[3]);
+
+			}
+
+
+		}
+		/*Enable receive interrupt*/
+		__HAL_UART_ENABLE_IT(&huart2, UART_IT_RXNE);
+		HAL_UART_Receive_IT (&huart2, Rx_buffer, 4);
+	}
+
+/*Increment the timer */
+/*This function is called in stm32f4xx_hal.c in HAL_IncTick function() */
+void IncrementTimer(void)
+{
+	if(SendFlag == 1)
+	{
+	TimeStamp = TimeStamp+1;
+	}
+}
+
 
 /* USER CODE END 0 */
 
@@ -94,6 +184,15 @@ int main(void)
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
 
+  /*Initialize the accelerometer*/
+  AccelInit ();
+
+  /*Enable the uart receive interrupt */
+   __HAL_UART_ENABLE_IT(&huart2, UART_IT_RXNE);
+   HAL_UART_Receive_IT (&huart2, Rx_buffer, 4);
+
+
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -101,6 +200,39 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
+		//  Printf("Print: AR=%s\r\n", "testing");
+	      /*If send flag is 0 collect the received data in Rx_buffer */
+	      if(SendFlag == 0)
+	  	  {
+	  		  HAL_UART_Receive_IT (&huart2, Rx_buffer, 4);
+	  	  }
+	  	  else if(SendFlag == 1)
+	  	  {
+	  		  /*Read the accelerometer values  */
+	  		  /*2 bytes represent the one single value for one axis for example x axis */
+	  		  /*So we have 6 bytes for x , y and z axis respectively*/
+	  		    AccelReadValues (0x32 , 6);
+
+	            /*Convert the time stamp value again to bytes */
+	            TS_buffer[3] = (uint8_t) ((TimeStamp >> 0)  & 0x000000FF);
+	  		    TS_buffer[2] = (uint8_t) ((TimeStamp >> 8)  & 0x000000FF);
+	  		    TS_buffer[1] = (uint8_t) ((TimeStamp >> 16) & 0x000000FF);
+	  		    TS_buffer[0] = (uint8_t) ((TimeStamp >> 24) & 0x000000FF);
+
+	            /*Calculate the checksum of 4 bytes of time stamp and 6 bytes of accelerometr*/
+	            CheckSum = TS_buffer[0] + TS_buffer[1] + TS_buffer[2] + TS_buffer[3] + AccelData[0]
+					   + AccelData[1]+ AccelData[2]+ AccelData[3]+ AccelData[4]+ AccelData[5];
+
+	            HAL_Delay(20);
+
+	            /*Transmit 4 bytes of TS_buffer*/
+	            HAL_UART_Transmit(&huart2 ,TS_buffer ,4 ,10);
+	            /*Transmit 6 bytes of Accelerometr Data*/
+	            HAL_UART_Transmit(&huart2 ,AccelData ,6 ,10);
+	            /*Transmit 1 byte of checksum*/
+	  		    HAL_UART_Transmit(&huart2 ,&CheckSum ,1 ,10);
+
+	  	}
 
     /* USER CODE BEGIN 3 */
   }
